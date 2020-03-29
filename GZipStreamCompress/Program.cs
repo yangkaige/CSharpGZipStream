48eb7f1b11ec2b122d10c08b96f0569eee0ba590
yangkaige <1546865912@qq.com>
2020-03-29 10:11:58 +0800

    文件压缩与解压功能

---
+++ GZipStreamCompress/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace GZipStreamCompress
+{
+    class Program
+    {
+        /// <summary>
+        /// 文件压缩
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            using (FileStream fsRead = File.OpenRead("1.txt"))
+            {
+                using (FileStream fsWrite = File.OpenWrite("yasuo.zip"))
+                {
+                    using (GZipStream gZipStream = new GZipStream(fsWrite, CompressionMode.Compress))
+                    {
+                        int r;
+                        byte[] buffer = new byte[1024 * 1024 * 5];
+                        while ((r = fsRead.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            gZipStream.Write(buffer, 0, r);
+                        }
+                    }
+                }
+
+            }
+            Console.WriteLine("压缩成功！！");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GZipStreamDecompress/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+namespace GZipStreamDecompress
+{
+    class Program
+    {
+        /// <summary>
+        /// 文件解压
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            using (FileStream fsRead = File.OpenRead("yasuo.txt"))
+            {
+                using (FileStream fsWrite = File.OpenWrite("1.txt"))
+                {
+                    using (GZipStream gZipStream = new GZipStream(fsRead, CompressionMode.Decompress))
+                    {
+                        int r;
+                        byte[] buffer = new byte[1024 * 1024 * 5];
+                        while ((r = gZipStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fsWrite.Write(buffer, 0, r);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("解压成功！！");
+            Console.ReadKey();
+        }
+    }
+}
